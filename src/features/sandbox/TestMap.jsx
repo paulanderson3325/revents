bafbbe57f00c14135c801a7f2a3a52e4175567a5
Paul Anderson <panderson@tiresoft.com>
2020-08-19 13:04:54 -0500

    End of section 8

---
+++ src/features/sandbox/TestMap.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import GoogleMapReact from "google-map-react"
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>
+
+export default function TestMap({ location }) {
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyDYm4upiitxhe_53g8QIpcolpytyC-cQFU" }}
+        center={location.center}
+        zoom={location.zoom}
+      >
+        <AnyReactComponent
+          lat={location.center.lat}
+          lng={location.center.lng}
+          text='My Marker'
+        />
+      </GoogleMapReact>
+    </div>
+  )
+}
