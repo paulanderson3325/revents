6763a1e781de923a5c663d8b076f63e330ea7297
Paul Anderson <panderson@tiresoft.com>
2020-08-21 15:34:19 -0500

    End of section 9

--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -8,7 +8,9 @@ import TestMap from "./TestMap"
 
 export default function Sandbox() {
   const dispatch = useDispatch()
+  const [target, setTarget] = useState(null)
   const data = useSelector((state) => state.test.data)
+  const { loading } = useSelector((state) => state.async)
   const defaultProps = {
     center: {
       lat: 59.95,
@@ -27,12 +29,22 @@ export default function Sandbox() {
       <h1>Testing 123</h1>
       <h3>The data is: {data}</h3>
       <Button
-        onClick={() => dispatch(increment(10))}
+        name='increment'
+        loading={loading && target === 'increment'}
+        onClick={(e) => {
+          dispatch(increment(10))
+          setTarget(e.target.name)
+        }}
         content='Increment'
         color='green'
       />
       <Button
-        onClick={() => dispatch(decrement(5))}
+        name='decrement'
+        loading={loading && target === 'decrement'}
+        onClick={(e) => {
+          dispatch(decrement(5))
+          setTarget(e.target.name)
+        }}
         content='Decrement'
         color='red'
       />
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "semantic-ui-css/semantic.min.css"
+import "react-toastify/dist/ReactToastify.min.css"
+import "react-calendar/dist/Calendar.css"
 import "./app/layout/styles.css"
 import ScrollToTop from "./app/layout/ScrollToTop"
 import App from "./app/layout/App"
@@ -8,8 +10,11 @@ import * as serviceWorker from "./serviceWorker"
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 import { configureStore } from "./app/store/configureStore"
+import { loadEvents } from "./features/events/eventActions"
 
 const store = configureStore()
+
+store.dispatch(loadEvents())
 
 ReactDOM.render(
   <Provider store={store}>
